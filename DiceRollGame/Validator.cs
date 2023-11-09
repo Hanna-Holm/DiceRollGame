ba56c4983171f4a698576ee48a66cf15fe4e1c2f
Hanna Holm <hanna.holm@hotmail.com>
2023-11-09 07:49:23 +0100

    Removed method in Validator.cs
    Not using this method anymore so I removed it as it is no longer neccessary.

--- DiceRollGame/Validator.cs
+++ DiceRollGame/Validator.cs
@@ -2,16 +2,6 @@
 {
     public class Validator
     {
-        public bool isGuessValid(string text)
-        {
-            bool isNumber = int.TryParse(text, out int result);
-
-            if (isNumber)
-                return true;
-
-            return false;
-        }
-
         public bool isGuessCorrect(string guess, int rolledNumber)
         {
             int.TryParse(guess, out int number);
